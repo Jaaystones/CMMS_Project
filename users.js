7cd7f23ef082897d5fe8f52aae2054eed904406f
Jaaystones <Joelpaul345@gmail.com>
2023-08-09 03:09:16 +0100

    :white_check_mark:Database table class

---
+++ users.js
@@ -0,0 +1,14 @@
+class Users {
+    constructor(Id, First_name, Last_name, Username, Password, Email, Phone_number, City) {
+        this.Id = Id;
+        this.First_name = First_name;
+        this.Last_name = Last_name;
+        this.Username = Username;
+        this.Password = Password;
+        this.Email = Email;
+        this.Phone_number = Phone_number;
+        this.City = City;
+    }
+}
+
+export default Users; // Export using ES6 module syntax
