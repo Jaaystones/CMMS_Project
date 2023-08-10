0838f19449b2a7420d156d81ac7b57131618288d
Jaaystones <Joelpaul345@gmail.com>
2023-08-10 13:08:20 +0100

    :wrench:Hardware database schema

---
+++ hardware.js
@@ -0,0 +1,13 @@
+class Hardware {
+    constructor(HwId, hwName, hwDesc, purchaseDesc, purchaseDate, distributor) {
+        this.HwId = HwId;
+        this.hwName = hwName;
+        this.hwDesc = hwDesc;
+        this.purchaseDesc = purchaseDesc;
+        this.purchaseDate = purchaseDate;
+        this.distributor = distributor;
+
+    }
+}
+
+export { Hardware };
