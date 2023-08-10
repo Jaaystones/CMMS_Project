99dd8cc3231b501716beea051edc89e8f85195b7
Jaaystones <Joelpaul345@gmail.com>
2023-08-10 13:21:43 +0100

    :white_check_mark:Updated functions for Database

--- models/dboperations.js
+++ models/dboperations.js
@@ -32,4 +32,60 @@ async function getuser(id) {
     }
 }
 
-export { getusers, getuser };
+// hardware functions
+async function gethardwares() {
+    try {
+        const connection = await createConnection();
+        const [rows] = await connection.execute("SELECT * FROM hardware_assets");
+        connection.end();
+        return rows;
+    } catch (error) {
+        console.error(error);
+        throw new Error("An error occurred while fetching hardware.");
+    }
+}
+
+async function gethardware(id) {
+    try {
+        const connection = await createConnection();
+        const [rows] = await connection.execute("SELECT * FROM hardware_assets WHERE hwId = ?", [id]);
+        connection.end();
+        if (rows.length === 0) {
+            throw new Error(`Hardware with ID ${id} not found`);
+        }
+        return rows[0];
+    } catch (error) {
+        console.error(error);
+        throw new Error(`An error occurred while fetching hardware with ID ${id}`);
+    }
+}
+
+// software functions
+async function getsoftwares() {
+    try {
+        const connection = await createConnection();
+        const [rows] = await connection.execute("SELECT * FROM software_assets");
+        connection.end();
+        return rows;
+    } catch (error) {
+        console.error(error);
+        throw new Error("An error occurred while fetching softwares.");
+    }
+}
+
+async function getsoftware(id) {
+    try {
+        const connection = await createConnection();
+        const [rows] = await connection.execute("SELECT * FROM software_assets WHERE SwId = ?", [id]);
+        connection.end();
+        if (rows.length === 0) {
+            throw new Error(`software with ID ${id} not found`);
+        }
+        return rows[0];
+    } catch (error) {
+        console.error(error);
+        throw new Error(`An error occurred while fetching software with ID ${id}`);
+    }
+}
+
+export { getusers, getuser, gethardwares, gethardware, getsoftwares, getsoftware };
