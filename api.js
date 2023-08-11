810e800017a098ce3315800b832adb73d95dc04e
Jaaystones <Joelpaul345@gmail.com>
2023-08-12 00:41:36 +0100

    :white_check_mark:Updated Post requests and Db configurations

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import * as dboperations from './models/dboperations.js';
+import * as db_softwareOps from './models/db_softwareOps.js';
+import * as db_usersOps from './models/db_usersOps.js';
+import * as db_hardwareOps from './models/db_hardwareOps.js';
 
 const app = express();
 const router = express.Router();
@@ -19,7 +21,7 @@ router.use((request, response, next) => {
 //Users Get requests
 router.route('/users').get(async (request, response) => {
     try {
-        const result = await dboperations.getusers();
+        const result = await db_usersOps.getusers();
         response.json(result);
     } catch (error) {
         response.status(500).json({ error: error.message });
@@ -29,7 +31,7 @@ router.route('/users').get(async (request, response) => {
 router.route('/users/:id').get(async (request, response) => {
     try {
         const id = request.params.id;
-        const user = await dboperations.getuser(id);
+        const user = await db_usersOps.getuser(id);
         response.json(user);
     } catch (error) {
         response.status(500).json({ error: error.message });
@@ -37,10 +39,9 @@ router.route('/users/:id').get(async (request, response) => {
 });
 
 //hardware get requests
-
 router.route('/hardware').get(async (request, response) => {
     try {
-        const result = await dboperations.gethardwares();
+        const result = await db_hardwareOps.gethardwares();
         response.json(result);
     } catch (error) {
         response.status(500).json({ error: error.message });
@@ -50,7 +51,7 @@ router.route('/hardware').get(async (request, response) => {
 router.route('/hardware/:id').get(async (request, response) => {
     try {
         const id = request.params.id;
-        const user = await dboperations.gethardware(id);
+        const user = await db_hardwareOps.gethardware(id);
         response.json(user);
     } catch (error) {
         response.status(500).json({ error: error.message });
@@ -60,7 +61,7 @@ router.route('/hardware/:id').get(async (request, response) => {
 //get functionn for software
 router.route('/software').get(async (request, response) => {
     try {
-        const result = await dboperations.getsoftwares();
+        const result = await db_softwareOps.getsoftwares();
         response.json(result);
     } catch (error) {
         response.status(500).json({ error: error.message });
@@ -70,8 +71,46 @@ router.route('/software').get(async (request, response) => {
 router.route('/software/:id').get(async (request, response) => {
     try {
         const id = request.params.id;
-        const user = await dboperations.getsoftware(id);
+        const user = await db_softwareOps.getsoftware(id);
         response.json(user);
+    } catch (error) {
+        response.status(500).json({ error: error.message });
+    }
+});
+
+// Post requests
+//Users post request for creating a new User
+router.route('/users').post(async (request, response) => {
+    try {
+        const newUser = { ...request.body };
+        const result = await db_usersOps.createUser(newUser);
+        response.setHeader('Content-Type', 'application/json');
+        response.status(201).json(result);
+    } catch (error) {
+        response.status(500).json({ error: error.message });
+    }
+});
+
+
+// Hardware POST request for creating a new hardware asset
+router.route('/hardware').post(async (request, response) => {
+    try {
+        const newHardware = { ...request.body };
+        const result = await db_hardwareOps.createHardware(newHardware);
+        response.setHeader('Content-Type', 'application/json');
+        response.status(201).json(result);
+    } catch (error) {
+        response.status(500).json({ error: error.message });
+    }
+});
+
+// Software Post request
+router.route('/software').post(async (request, response) => {
+    try {
+        const newSoftware = { ...request.body };
+        const result = await db_softwareOps.createSoftware(newSoftware);
+        response.setHeader('Content-Type', 'application/json');
+        response.status(201).json(result);
     } catch (error) {
         response.status(500).json({ error: error.message });
     }
