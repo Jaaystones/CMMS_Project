2c26a275cdd8baa874cc4fc5a4a66bf62e5fe28e
Jaaystones <Joelpaul345@gmail.com>
2023-08-20 21:52:42 +0100

    :wrench:Database connection

--- models/dbconfig.js
+++ models/dbconfig.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const connectionConfig = {
-    user: "root",
-    password: "Jogolo4sokka",
-    host: "localhost",
-    database: "cmms",
-    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.MYSQL_DB,
+    port: parseInt(process.env.DB_PORT),
 };
 
 export default connectionConfig;
