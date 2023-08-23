e5ffd71cde4a92c41fcde1200dba2a2a2a0fd2e3
Jaaystones <Joelpaul345@gmail.com>
2023-08-23 17:09:30 +0100

    :package:Authentication operations for login and registration

--- models/db_auth.js
+++ models/db_auth.js
@@ -1,63 +1,78 @@
-//import mysql from 'mysql2/promise';
-//import connectionConfig from './dbconfig.js';
+import mysql from 'mysql2/promise';
+import connectionConfig from './dbconfig.js';
 import bcrypt from 'bcrypt';
 import { createError } from '../auth/error.js';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import * as db_usersOps from './db_usersOps.js';
 
 const secretKey = process.env.JWT_SECRET_KEY;
 
-//Create a connection
-//async function createConnection() {
- //   return await mysql.createConnection(connectionConfig);
-//}
+// Create a connection
+async function createConnection() {
+  return await mysql.createConnection(connectionConfig);
+}
 
 const register = async (req, res, next) => {
-    try{
-        //const connection = await createConnection;
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.Password, salt);
+  try {
+    const connection = await createConnection();
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.Password, salt);
 
-        const newUser = new db_usersOps.createUser ({
-            ...req.body,
-            password: hash,
-        });
+    const newUser = {
+      ...req.body,
+      Password: hash,
+    };
 
-        await newUser.save();
-        res.status(200).send('User has been created');
-    } catch (err) {
-        next(err);
-    }
-}
-// get username
-//log in user
-const login = async (req, res, next) => {
-    try{
-        const user = await db_usersOps.findOne({ username: req.body.username });
-        if (!user) return next(createError(404, 'User not found!'));
+    // Call the createUser function from db_usersOps module
+    await db_usersOps.createUser(newUser);
+    connection.end();
 
-        const isPasswordCorrect = await bcrypt.compare(
-            req.body.Password,
-            user.Password
-        );
-        if (!isPasswordCorrect)
-        return next(createError(404, 'Invalid Username or Password'));
-        
-        const token = jwt.sign(
-            { id: user._id, isAdmin: user.isAdmin },
-            secretKey
-        );
+    res.status(200).json({
+      success: true,
+      message: 'User Created!',
+      token: token
+    });
 
-        const { password, isAdmin, ...otherDetails } = user._doc;
-        res
-          .cookie("access_token", token, {
-            httpOnly: true,
-      })
-      .status(200)
-      .json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
-}
+};
 
-export { register, login};
+const login = async (req, res, next) => {
+  try {
+    const connection = await createConnection();
+    const user = await db_usersOps.getusersName(req.body.Username);
+    await connection.end();
+    if (!user) {
+      return next(createError(400, 'Invalid request. Username not valid.'));
+  }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.Password,
+      user.Password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(404, 'Invalid Username or Password'));
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      secretKey
+    );
+
+    const { password, isAdmin, ...otherDetails } = user;
+    res.cookie('access_token', token, {
+        httpOnly: true,
+      });
+      res.status(200).json({
+        success: true,
+        message: 'Login successful.',
+        details: otherDetails,
+        isAdmin: user.isAdmin,
+        token: token
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { register, login };
