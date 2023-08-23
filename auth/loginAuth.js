0690a447bc57a27052cff37777f7bac8958e9ddd
Jaaystones <Joelpaul345@gmail.com>
2023-08-23 16:54:42 +0100

    :wrench: Authentication Files to verify routes

--- auth/loginAuth.js
+++ auth/loginAuth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { createError } from './auth/error.js';
+import { createError } from '../auth/error.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,7 +8,7 @@ const secretKey = process.env.JWT_SECRET_KEY;
 const checkToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if (!token) {
-        return next(createError(401, 'You aree not authorized' ));
+        return next(createError(401, 'You are not authorized!'));
     }
 
     jwt.verify(token, secretKey, (err, decoded) => {
@@ -19,11 +19,13 @@ const checkToken = (req, res, next) => {
 };
 
 const verifyUser = (req, res, next) => {
+    checkToken(req, res, next, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
         next();
     } else {
         return next(createError(403, 'You are not Authorized'));
     }
-};
+});
+}
 
 export { checkToken, verifyUser };
